cb4bb8d99c36260e88e6e36a71472fba80cca4f6
mbaibatyr <murat_b@mail.ru>
2024-05-19 15:52:34 +0500

    --

--- MyTask/Program.cs
+++ MyTask/Program.cs
@@ -10,27 +10,56 @@ namespace MyTask
             for (int i = 1; i <= 5; i++)
             {
                 Console.WriteLine(i.ToString());
-                Thread.Sleep(100);
+                //Thread.Sleep(100);
             }
         }
 
+        static void Test2()
+        {
+            Console.WriteLine("Hello STEP 1");
+        }        
+
+        static async Task Test2_CallAsync()
+        {
+            //Task.Delay(1000);
+            await Task.Run(() =>
+                Test2()
+            );
+
+            using (SqlConnection db = new SqlConnection())
+            {
+                db.Query<dynamic>("");
+            }
+            
+
+        }
+
         static int Sum(int a, int b)
-        {            
+        {
             return a + b;
         }
-        static void Main(string[] args)
-        {            
-            var task1 = new Task<int>(() =>            
-                Sum(10,20)
-            );            
-            task1.Start();
-            task1.Wait();
-            Console.WriteLine(task1.Result);
+
+        async static Task<int> Sum2(int a, int b)
+        {
+            return a + b;
+        }
+        static async Task Main(string[] args)
+        {
+            await Test2_CallAsync();
 
 
+            //Console.WriteLine("Hello STEP 2");
+            //Console.WriteLine(await Sum2(12, 13));
+            //var task1 = new Task<int>(() =>            
+            //    Sum(10,20)
+            //);            
+            //task1.Start();
+            //task1.Wait();
+            //Console.WriteLine(task1.Result);
 
             //Task.Run(() =>
             //{
+            //    Test1();
             //    Test1();
             //    Test1();
             //});
@@ -41,12 +70,12 @@ namespace MyTask
             //});
             //task1.Start();
 
-            //Task[] arTasks = new Task[5]
+            //Task[] arTasks = new Task[2]
             //{
             //    new Task(() => Test1()),
-            //    new Task(() => Test1()),
-            //    new Task(() => Test1()),
-            //    new Task(() => Test1()),
+            //    //new Task(() => Test1()),
+            //    //new Task(() => Test1()),
+            //    //new Task(() => Test1()),
             //    new Task(() => Test1())
             //};
 
