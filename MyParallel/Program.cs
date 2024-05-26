9bfa297e475baa61f7057e49d585b7283b2f2164
mbaibatyr <murat_b@mail.ru>
2024-05-26 15:59:30 +0500

    --

---
+++ MyParallel/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MyParallel
+{
+    internal class Program
+    {
+        static void Test(int k)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"i = {i}, ThreadId = {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(200);
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Parallel.For(1, 5, Test);            
+            //Parallel.For(1, 5, k =>
+            //{
+            //    for (int i = 1; i <= 10; i++)
+            //    {
+            //        Console.WriteLine($"i = {i}, ThreadId = {Thread.CurrentThread.ManagedThreadId}");
+            //        Thread.Sleep(200);
+            //    }
+            //});
+
+            Parallel.ForEach<int>(new List<int> { 1, 10 }, Test);
+        }
+    }
+}
---
+++ MyTimer/Program.cs
@@ -0,0 +1,51 @@
+﻿using T1 = System.Timers;
+using T2 = System.Threading;
+
+namespace MyTimer
+{
+    internal class Program
+    {
+        static int Counter = 0;
+        static T1.Timer timer_1;
+        static T2.Timer timer_2;       
+
+        static void Main(string[] args)
+        {
+            //TimerCallback tc = new TimerCallback(Method2);
+            //timer_2 = new T2.Timer(tc, 2, 5, 300);
+            //Console.ReadLine();
+
+            timer_1 = new T1.Timer();
+            timer_1.Interval = 300;
+            timer_1.Elapsed += Method1;            
+            timer_1.Start();
+            string st = "start";
+            while (st == "start")
+            {
+                st = Console.ReadLine();
+                if (st == "start")
+                {
+                    Counter = 0;
+                    timer_1.Start();
+                }
+            }
+
+        }
+
+        static async void Method1(object sender, T1.ElapsedEventArgs e)
+        {            
+            Console.WriteLine("Hello STEP - " + Counter++);
+            if(Counter > 10)
+                timer_1.Stop();            
+                //timer_1.Enabled = false;
+                //timer_1.Elapsed -= Method1;
+        }
+
+        static void Method2(object sender)
+        {
+            Console.WriteLine("Hello STEP - " + Counter++);
+            if (Counter > 10)                
+                timer_2.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+    }
+}
